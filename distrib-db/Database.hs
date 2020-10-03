108a6606181724a1cdac9580dd9f80f47c145f8d
Shaun Lee <shaun.lee@gmail.com>
2020-10-03 10:36:31 -0700

    Redo Part 1 to use channels properly.

--- distrib-db/Database.hs
+++ distrib-db/Database.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RecordWildCards #-}
 module Database (
        Database,
@@ -9,33 +11,68 @@ module Database (
 
 import Control.Concurrent.STM
 import Control.Distributed.Process
+import Control.Distributed.Process.Closure
+import Control.Monad (forever)
 
+import Data.Binary (Binary)
+import Data.Typeable
 import qualified Data.Map as Map
 import Data.Map (Map)
+import GHC.Generics
+
+import Debug.Trace (trace)
 
 type Key   = String
 type Value = String
 
-data Database = Database { kvStore :: TVar (Map Key Value), spid :: ProcessId }
+type Database = ProcessId
+
+type Client = ProcessId
+
+data DbMessage = Set Client Key Value | Get Client Key | Response (Maybe Value)
+    deriving ( Typeable, Generic )
+
+instance Binary DbMessage
+
+dbHandleMessage :: TVar (Map Key Value) -> DbMessage -> Process ()
+dbHandleMessage kvStore (Set _ k v) = liftIO $ atomically $ do
+  kvs <- readTVar kvStore
+  writeTVar kvStore (Map.insert k v kvs)
+dbHandleMessage kvStore (Get c k) = do
+  v <- liftIO $ atomically $ do
+    kvs <- readTVar kvStore
+    return $ Map.lookup k kvs
+  send c (Response v)
+
+dbServer :: Process ()
+dbServer = do
+  kvs <- liftIO $ newTVarIO Map.empty
+  forever $ do
+    m <- expect :: Process DbMessage
+    dbHandleMessage kvs m
+
+remotable ['dbServer]
 
 createDB :: [NodeId] -> Process Database
 createDB nodes = do
-  pid <- getSelfPid
-  liftIO $ do
-    kvs <- newTVarIO Map.empty
-    return Database { kvStore = kvs, spid = pid}
+  node <- getSelfNode
+  pid <- spawn node $(mkStaticClosure 'dbServer)
+  return pid
 
 set :: Database -> Key -> Value -> Process ()
-set Database{..} k v = liftIO $ atomically $ do
-  kvs <- readTVar kvStore
-  writeTVar kvStore (Map.insert k v kvs)
+set db k v = do
+  pid <- getSelfPid
+  send db (Set pid k v)
+  return ()
 
 get :: Database -> Key -> Process (Maybe Value)
-get db k = liftIO $ atomically $ do
-  kvs <- readTVar (kvStore db)
-  return $ Map.lookup k kvs
+get db k = do
+  pid <- getSelfPid
+  send db (Get pid k)
+  Response resp <- expect
+  return resp
 
 rcdata :: RemoteTable -> RemoteTable
-rcdata = id
+rcdata = Database.__remoteTable
   -- For the exercise, change this to include your
   -- remote metadata, e.g. rcdata = Database.__remoteTable
